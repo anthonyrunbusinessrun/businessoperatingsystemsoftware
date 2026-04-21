17c1cefa3c0f11d794b5413bed457df7af94bdda
anthonyrunbusinessrun <anthony@runbusiness.com>
2026-04-21 22:58:06 +0800

    feat: add _layout.tsx

---
+++ apps/mobile/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack screenOptions={{
+        headerStyle: { backgroundColor: "#212c42" },
+        headerTintColor: "#ffffff",
+        headerTitleStyle: { fontWeight: "bold" }
+      }}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
+  );
+}
